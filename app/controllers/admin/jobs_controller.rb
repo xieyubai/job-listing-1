ae69da88cb332ecb2747a9aea81e49a24a4b552c
xieyubai <xieyubai17@gmail.coml>
2017-01-13 22:17:33 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -3,13 +3,6 @@ class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = '你不是管理员账号'
-      redirect_to root_path
-    end
-  end
-
   def show
     @job = Job.find(params[:id])
   end
@@ -56,6 +49,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description,:wage_upper_bound, :wage_lower_bound, :contact_email )
   end
 end
