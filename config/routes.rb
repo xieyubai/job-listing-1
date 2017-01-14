3c20c18d80415536ac0bfc16804baa0265527ff7
xieyubai <xieyubai17@gmail.coml>
2017-01-15 03:43:15 +0800

    admin can see resume list“ ” “”

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :resumes
  end
 
+
     resources :jobs
     root 'jobs#index'
 
@@ -14,6 +15,7 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
 end
