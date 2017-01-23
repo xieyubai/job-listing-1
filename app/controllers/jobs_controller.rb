50bac7c9ee4732dc70845e4656b9f82d8ebf8d2e
xieyubai <xieyubai17@gmail.coml>
2017-01-23 17:09:56 +0800

    add index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,7 +16,7 @@ class JobsController < ApplicationController
              when 'by_upper_bound'
                Job.published.order('wage_upper_bound DESC')
              else
-               Job.published.recent
+               Job.published.recent.search(params[:search])
              end
   end
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -16,4 +16,8 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
+
+  def self.search(search)
+      where("title LIKE ?", "%#{search}%").or(where("description LIKE ?", "%#{search}%"))
+   end
 end
